55da8aab1f8e8b9cbed1ce9c7ea2be5681fdddfb
VitorBCC007 <118393027+VitorBCC007@users.noreply.github.com>
2023-06-14 23:11:27 -0300

    Update Animais.py

--- RedeNeural/Animais.py
+++ RedeNeural/Animais.py
@@ -11,6 +11,20 @@ cifar10 = keras.datasets.cifar10
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+# Função para adicionar ruído gaussiano a uma imagem
+def add_gaussian_noise(image, std_dev):
+    noise = np.random.normal(loc=0, scale=std_dev, size=image.shape)
+    noisy_image = image + noise
+    noisy_image = np.clip(noisy_image, 0.0, 1.0)  # Garantir que os valores dos pixels estejam entre 0 e 1
+    return noisy_image
+
+# Adicionar ruído gaussiano às imagens de treinamento
+std_dev = 0.0  # Desvio padrão do ruído gaussiano
+noisy_train_images = np.array([add_gaussian_noise(image, std_dev) for image in train_images])
+
+# Adicionar ruído gaussiano às imagens de teste
+noisy_test_images = np.array([add_gaussian_noise(image, std_dev) for image in test_images])
+
 # Definir a arquitetura da rede neural
 model = keras.Sequential([
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
@@ -25,26 +39,26 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Treinar a rede neural
-model.fit(train_images, train_labels, epochs=1)
+# Treinar a rede neural com as imagens ruidosas
+model.fit(noisy_train_images, train_labels, epochs=1)
 
-# Avaliar a acurácia do modelo
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+# Avaliar a acurácia do modelo usando as imagens de teste ruidosas
+test_loss, test_acc = model.evaluate(noisy_test_images, test_labels)
 print("Acurácia do teste:", test_acc)
 
 # Fazer previsões com o modelo treinado
-predictions = model.predict(test_images)
+predictions = model.predict(noisy_test_images)
 
-# Exibir algumas imagens de teste e suas previsões
+# Exibir algumas imagens de teste ruidosas e suas previsões
 class_names = ['Avião', 'Automóvel', 'Pássaro', 'Gato', 'Veado', 'Cachorro', 'Sapo', 'Cavalo', 'Navio', 'Caminhão']
 
 plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i+1)
+for i in range(16):
+    plt.subplot(4, 4, i+1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
-    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.imshow(noisy_test_images[i], cmap=plt.cm.binary)
     predicted_label = np.argmax(predictions[i])
     true_label = test_labels[i][0]
     if predicted_label == true_label:
